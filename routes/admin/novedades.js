c4d4f5e3959fb78e7f919a8b80ddf3647cdf3403
PauSangui <paula.sanguineti@hotmail.com>
2022-06-20 10:06:38 -0300

    cambie ''usuariosModels.js'' por ''usuarioModel.js''

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var usuariosModel = require('../../models/usuariosModels');
+var usuariosModel = require('../../models/usuariosModel');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
